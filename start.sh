9c7849b5acf8cc251de7d98b18b1a37e1adbd2e6
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 15:45:42 +0300

    add coqui_tts

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/zsh
-sudo touch ./output.log
-sudo chmod 777 ./output.log
-sudo chmod 733 ./server.py
-sudo nohup python3 ./server.py > ./output.log &
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+touch ./output.log
+chmod 777 ./output.log
+chmod 733 ./server.py
+nohup python3 ./server.py > ./output.log &
