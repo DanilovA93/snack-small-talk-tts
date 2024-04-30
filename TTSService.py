8b026c4290b8aba24ef512285a65c15166d92b12
DanilovA93 <danilovanton1993@mail.ru>
2024-04-30 23:17:10 +0300

    change config

--- TTSService.py
+++ TTSService.py
@@ -11,22 +11,31 @@ TOKENIZER_PATH =            "./resources/xtts/vocab.json"
 SPEAKER_PATH =              "./resources/xtts/speakers_xtts.pth"
 SPEAKER_REFERENCE_PATH =    "./resources/samples/female_sample.wav"
 
-print("Loading model...")
+LANGUAGE = "en"
+
+print("Reading config...")
 config = XttsConfig()
 config.load_json(CONFIG_PATH)
+
+print("Loading model...")
 model = Xtts.init_from_config(config)
 model.load_checkpoint(
     config,
     checkpoint_path=CHECKPOINT_PATH,
     vocab_path=TOKENIZER_PATH,
-    speaker_file_path=SPEAKER_PATH,
+    # speaker_file_path=SPEAKER_PATH,
     eval=True,
-    # use_deepspeed=True,
+    use_deepspeed=True,
 )
 model.cuda()
 
 print("Computing speaker latents...")
-gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=[SPEAKER_REFERENCE_PATH])
+gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(
+    audio_path=[SPEAKER_REFERENCE_PATH],
+    gpt_cond_len=30,
+    gpt_cond_chunk_len=4,
+    max_ref_length=60
+)
 
 print("GPT service is ready")
 
@@ -39,7 +48,7 @@ def process(
     print("Processing...")
     out = model.inference(
         prompt,
-        language="en",
+        language=LANGUAGE,
         gpt_cond_latent=gpt_cond_latent,
         speaker_embedding=speaker_embedding,
         temperature=temperature,
